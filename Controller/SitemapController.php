7b44547cc5c049485e098671ce84325205ef92f9
kingstar <admin@baks.dev>
2023-11-02 16:33:56 +0300

    Baks Development

---
+++ Controller/SitemapController.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+namespace BaksDev\Products\Category\Controller;
+
+use BaksDev\Core\Controller\AbstractController;
+use BaksDev\Core\Listeners\Event\Security\RoleSecurity;
+use BaksDev\Products\Category\Entity\ProductCategory;
+use BaksDev\Products\Category\Repository\AllCategoryByMenu\AllCategoryByMenuInterface;
+use ReflectionAttribute;
+use ReflectionClass;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+
+#[AsController]
+final class SitemapController extends AbstractController
+{
+    /**
+     * Карта на разделы и продукцию
+     */
+    #[Route('/sitemaps/catalog/sitemap.xml', name: 'sitemap', methods: ['GET'])]
+    public function sitemap(AllCategoryByMenuInterface $allCategory): Response
+    {
+        $category = $allCategory->fetchAllCatalogMenuAssociative();
+        $response = $this->render(['category' => $category]);
+        $response->headers->set('Content-Type', 'text/xml');
+
+        return $response;
+    }
+
+
+    /**
+     * Ссылки на разделы
+     */
+    #[Route('/sitemap/catalog/urls.xml', name: 'sitemap.urls', methods: ['GET'])]
+    public function urls(AllCategoryByMenuInterface $allCategory): Response
+    {
+
+        $category = $allCategory->fetchAllCatalogMenuAssociative();
+
+        $response = $this->render(['urls' => $category]);
+        $response->headers->set('Content-Type', 'text/xml');
+
+        return $response;
+    }
+
+
+}
