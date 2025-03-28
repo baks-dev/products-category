08ff31145dcf17ab7fcb4ccf1ece1c4f5d1956ee
kingstar <admin@baks.dev>
2025-03-29 01:52:13 +0300

    Baks Development

--- UseCase/Admin/NewEdit/CategoryProductHandler.php
+++ UseCase/Admin/NewEdit/CategoryProductHandler.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -94,10 +94,12 @@ final class CategoryProductHandler extends AbstractHandler
         }
 
 
-        $this->entityManager->flush();
+        $this->flush();
 
         /* Отправляем событие в шину  */
-        $this->messageDispatch->dispatch(
+        $this->messageDispatch
+            ->addClearCacheOther('products-product')
+            ->dispatch(
             message: new ProductCategoryMessage($this->main->getId(), $this->main->getEvent(), $command->getEvent()),
             transport: 'products-category'
         );
