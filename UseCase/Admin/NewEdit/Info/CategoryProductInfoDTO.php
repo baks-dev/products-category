903ef876cfe75c413da227389e5f30d116bf8740
MichelAngelo <baksdevelopmen@gmail.com>
2025-07-01 11:17:41 +0300

    BaksDevelopment

--- UseCase/Admin/NewEdit/Info/CategoryProductInfoDTO.php
+++ UseCase/Admin/NewEdit/Info/CategoryProductInfoDTO.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,56 +37,6 @@ final class CategoryProductInfoDTO implements CategoryProductInfoInterface
 
     /** Статус активности раздела */
     private bool $active = false;
-
-
-    /** Минимальное количество в заказе ниже которого запрещается оформить заказ */
-    #[Assert\NotBlank]
-    private int $minimal = 1;
-
-    /** Количество по умолчанию (предзаполняет форму) */
-    #[Assert\NotBlank]
-    private int $input = 1;
-
-    /** Порог наличия продукции (default 10) @example «более 10» | «менее 10» */
-    #[Assert\NotBlank]
-    private int $threshold = 10;
-
-    public function getMinimal(): int
-    {
-        return $this->minimal;
-    }
-
-    public function setMinimal(int $minimal): self
-    {
-        $this->minimal = $minimal;
-
-        return $this;
-    }
-
-    public function getInput(): int
-    {
-        return $this->input;
-    }
-
-    public function setInput(int $input): self
-    {
-        $this->input = $input;
-
-        return $this;
-    }
-
-    public function getThreshold(): int
-    {
-        return $this->threshold;
-    }
-
-    public function setThreshold(int $threshold): self
-    {
-        $this->threshold = $threshold;
-
-        return $this;
-    }
-
 
     /** Семантическая ссылка на раздел */
 
--- UseCase/Admin/NewEdit/Info/CategoryProductInfoForm.php
+++ UseCase/Admin/NewEdit/Info/CategoryProductInfoForm.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,10 +25,11 @@ namespace BaksDev\Products\Category\UseCase\Admin\NewEdit\Info;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class CategoryProductInfoForm extends AbstractType
 {
@@ -39,15 +40,6 @@ final class CategoryProductInfoForm extends AbstractType
 
         /** Статус активности раздела */
         $builder->add('active', CheckboxType::class, ['required' => false]);
-
-        /** Минимальное количество в заказе ниже которого запрещается оформить заказ */
-        $builder->add('minimal', IntegerType::class);
-
-        /** Количество по умолчанию (предзаполняет форму) */
-        $builder->add('input', IntegerType::class);
-
-        /** Порог наличия продукции (default 10) @example «более 10» | «менее 10» */
-        $builder->add('threshold', IntegerType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
